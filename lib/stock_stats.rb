8e69f86af7ff171c0a0592d7253d62a6fb6cb202
Jessica Kerr <jessitron@gmail.com>
2013-09-12 07:35:38 -0500

    Use rubyz Either in stock_stats

--- lib/stock_stats.rb
+++ lib/stock_stats.rb
@@ -4,12 +4,14 @@ require_relative 'book_in_stock'
 convert_row_to_book = ->(row) { BookInStock.from_row(row) }
 read_all_lines = ->(file) { CsvReader.new(file).to_a }
 
+def always(value); ->(a) { value } end
+fold_to_price = ->(either) { either.fold(always(0), ->(b) { b.price })}
+#fold_to_count_errors = ->(either) { either.fold(left: always(1), right: always(0))}
+
 all_results = ARGV.flat_map(&read_all_lines).map(&convert_row_to_book)
-all_books = all_results.select(&:right?).map(&:val)
+total = all_results.map(&fold_to_price).reduce(:+)
 all_errors = all_results.select(&:left?)
 
-total = BookInStock.sum_prices(all_books)
-
-puts("Total price on #{all_books.length} books: #{total}")
+puts("Total price on books: #{total}")
 puts("#{all_errors.length} lines were rejected")
 
